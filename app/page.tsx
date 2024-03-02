8679dad288efacebdcdc9bff7e58940d1a801744
reetbatra <reetbatra25@gmail.com>
2024-03-03 01:57:52 +0530

    in home page- creating all components camp, features, getapp, guide, hero

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,19 @@
+import Camp from '@/components/Camp'
+import Features from '@/components/Features'
+import Guide from '@/components/Guide'
+import Hero from '@/components/Hero'
 import Image from 'next/image'
+import GetApp from '@/components/GetApp'
 
 export default function Home(){
   return(
-    <h1 className="font-bold text-teal-500 text-2xl">
-    Hello world
-    </h1>
+    <>
+    <Hero />
+    <Camp />
+    <Guide />
+    <Features />
+    <GetApp/>
+      
+    </>
   )
 }
